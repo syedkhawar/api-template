96d820b80edfd582e6cc232a37556bd21a4f78a0
syed khawar <khawarshah@live.com>
2018-05-11 22:22:13 +0000

    add database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // factory(App\Model\Product::class, 200)->create();
+        // factory(App\Model\Review::class, 2000)->create();
+        // factory(App\Model\User::class, 30)->create();
     }
 }
